f74fdfe6b140152e276ab91a644988ebb8a39560
Charu <cxp111@student.bham.ac.uk>
2024-02-05 14:14:51 +0000

    Added personal info

--- portfolio-site/src/components/About.jsx
+++ portfolio-site/src/components/About.jsx
@@ -2,7 +2,29 @@ function About() {
 
     return (
       <>
-        
+        <h2>Personal Information</h2>
+        <p>
+          Hi, I'm Charu, a soon to be graduate from the University of Birmingham! On this site, I wanted to 
+          showcase my experience in a more hands-on way. Within software development, my passion truly lies with 
+          ensuring everything I produce is accessible for as many users as possible, whilst still being clean and
+          efficient.
+        </p>
+        <p>
+        Please take a look around:<br />
+        - the Education page will tell you more about previous projects I've worked on for University coursework, 
+        and the work I've undertaken as part of my apprenticeship with PwC. <br />
+        - the Skills page shows some of the programming languages and tools that I have used, along with any
+        other relevant qualifications.
+        </p>
+        <p>Link to my CV</p>
+        <h2>Contact Information</h2> 
+        <p>
+          Feel free to reach out in any of the following ways:<br />
+          - Email: charu.t.patel@gmail.com <br />
+          - LinkedIn: https://www.linkedin.com/in/charu-patel-207b59283/<br />
+          - GitHub: https://github.com/Ch4ruP
+
+        </p>
       </>
     )
   }
